c8b91e28ddc1fcb7c78b3de9b07fc912f289a9ab
nosdepjr <edson.ej100@gmail.com>
2026-02-06 01:32:32 -0300

    feat: cria primeira rota

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
@@ -0,0 +1,15 @@
+package dev.java10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller{
+
+    @GetMapping("/helloWorld")
+    public String helloWorld(){
+        return "Essa é a minha primeira rota. Hello World!";
+    }
+}
