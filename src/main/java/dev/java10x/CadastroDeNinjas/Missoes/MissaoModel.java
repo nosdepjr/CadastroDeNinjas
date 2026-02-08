4b243edf50841510091c1fe12f7e4acca616f6e5
nosdepjr <edson.ej100@gmail.com>
2026-02-08 13:06:54 -0300

    refactor: Adiciona Lombok para diminuir boilerplate

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoModel.java
@@ -2,11 +2,17 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissaoModel{
 
     @Id
@@ -17,28 +23,4 @@ public class MissaoModel{
     private String dificuldade;
     @OneToMany(mappedBy = "missao_id") // Uma missao pode ser designada para vários ninjas
     private List<NinjaModel> ninjas;
-
-    public MissaoModel(){
-    }
-
-    public MissaoModel(String nome, String dificuldade){
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-    }
-
-    public String getNome(){
-        return nome;
-    }
-
-    public void setNome(String nome){
-        this.nome = nome;
-    }
-
-    public String getDificuldade(){
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade){
-        this.dificuldade = dificuldade;
-    }
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,9 +2,15 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 
 import dev.java10x.CadastroDeNinjas.Missoes.MissaoModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_ninjas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,37 +22,4 @@ public class NinjaModel{
     @ManyToOne // Vários ninjas podem ter a mesma missao
     @JoinColumn(name = "missao") // Fk
     private MissaoModel missao;
-
-    public NinjaModel(){
-    }
-
-    public NinjaModel(String nome, String email, int idade){
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome(){
-        return nome;
-    }
-
-    public void setNome(String nome){
-        this.nome = nome;
-    }
-
-    public String getEmail(){
-        return email;
-    }
-
-    public void setEmail(String email){
-        this.email = email;
-    }
-
-    public int getIdade(){
-        return idade;
-    }
-
-    public void setIdade(int idade){
-        this.idade = idade;
-    }
 }
