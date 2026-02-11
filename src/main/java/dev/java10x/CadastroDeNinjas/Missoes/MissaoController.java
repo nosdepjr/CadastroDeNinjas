84a8d292b6948793b50e5fce67f9e69269961d82
nosdepjr <edson.ej100@gmail.com>
2026-02-11 12:52:33 -0300

    feat: cria mock de endpoints nos controllers de ninjas e missoes

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoController.java
@@ -1,15 +1,33 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/missao")
+@RequestMapping("/missoes")
 public class MissaoController{
-
+    // CRUD (Create, Read, Update, Delete)
     @GetMapping("/helloWorld")
     public String helloWorld(){
         return "Essa é a primeira rota das missoes.Hello World!";
+    }
+
+    @GetMapping("/listar")
+    public String listarMissoes() {
+        return "Missoes listadas com sucesso";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "Missao criada com sucesso";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "Missao alterada com sucesso";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao() {
+        return "Missao deletada com sucesso";
     }
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,15 +1,38 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/ninjas")
 public class NinjaController{
-
+    // CRUD (Create, Read, Update, Delete)
     @GetMapping("/helloWorld")
     public String helloWorld(){
         return "Essa é a minha primeira rota. Hello World!";
+    }
+
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja criado";
+    }
+
+    @GetMapping("/listar")
+    public String listarNinjas() {
+        return "Mostrar Ninja";
+    }
+
+    @GetMapping("/listarID")
+    public String listarNinjaPorId() {
+        return "Mostrar Ninja por id";
+    }
+
+    @PutMapping("/alterarID")
+    public String alterarNinjaPorId() {
+        return "Alterar Ninja por id";
+    }
+
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorId() {
+        return "Ninja deletado por id";
     }
 }
