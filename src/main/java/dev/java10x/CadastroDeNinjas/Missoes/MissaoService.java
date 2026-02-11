8fe443806d7cc3cc80cc26e24c3e3d7d8565814e
nosdepjr <edson.ej100@gmail.com>
2026-02-11 13:28:09 -0300

    feat: cria a camada de Service para as entidades Ninja e Missao

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoService.java
@@ -0,0 +1,7 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MissaoService{
+}
---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -0,0 +1,9 @@
+package dev.java10x.CadastroDeNinjas.Ninjas;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NinjaService{
+}
