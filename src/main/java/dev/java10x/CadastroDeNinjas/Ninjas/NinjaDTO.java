d93f7fdbfd54ee74638d043f37c6039695fa2709
nosdepjr <edson.ej100@gmail.com>
2026-02-13 14:33:56 -0300

    chore: implementa DTO e Mapper (mais uma camada de segurança adicionada)

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,20 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import dev.java10x.CadastroDeNinjas.Missoes.MissaoModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaDTO{
+
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String imgURL;
+    private String rank;
+    private MissaoModel missao;
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaMapper.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaMapper.java
@@ -1,4 +1,39 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper{
+
+    public NinjaDTO mapToDTO(NinjaModel model) {
+
+        if (model == null) {return null;}
+
+        NinjaDTO dto = new NinjaDTO();
+        dto.setId(model.getId());
+        dto.setNome(model.getNome());
+        dto.setEmail(model.getEmail());
+        dto.setIdade(model.getIdade());
+        dto.setImgURL(model.getImgURL());
+        dto.setRank(model.getRank());
+        dto.setMissao(model.getMissao());
+
+        return dto;
+    }
+
+    public NinjaModel mapToModel(NinjaDTO dto) {
+
+        if (dto == null) {return null;}
+
+        NinjaModel model = new NinjaModel();
+        model.setId(dto.getId());
+        model.setNome(dto.getNome());
+        model.setEmail(dto.getEmail());
+        model.setIdade(dto.getIdade());
+        model.setImgURL(dto.getImgURL());
+        model.setRank(dto.getRank());
+        model.setMissao(dto.getMissao());
+
+        return model;
+    }
 }
