6ac4915a24fbf5b5314f8b06e77a7f712cf89391
nosdepjr <edson.ej100@gmail.com>
2026-02-16 11:04:35 -0300

    chore: reorganiza projeto e integra Swagger - Atualiza pom.xml - Remove logs locais do BD - Ajustes de configuração

--- src/main/resources/db/migrations/V2__Add_rank_tb_ninjas.sql
+++
@@ -1,4 +0,0 @@
--- V2: Migration para adicionar a coluna de RANK na tabela de cadastro de ninjas
-
-ALTER TABLE tb_ninjas
-ADD COLUMN rank VARCHAR(255);
--- src/main/resources/db/migrations/V3__Add_img_url_tb_ninjas.sql
+++
@@ -1,4 +0,0 @@
--- V3: Migration para adicionar a coluna de IMG_URL na tabela de cadastro de ninjas
-
-ALTER TABLE tb_ninjas
-ADD COLUMN img_url VARCHAR(255);
